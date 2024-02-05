787253a087000feec77ed2a2598a4f0beb6255ec
yashpredator <yashrajsrivastava555@gmail.com>
2024-02-06 01:01:13 +0530

    Doctor profile Appointment done. Messages section remain

--- Sanjeevani/src/components/DocPersonal/Profile.jsx
+++ Sanjeevani/src/components/DocPersonal/Profile.jsx
@@ -5,74 +5,82 @@ const Profile = () => {
   const doctor = [
     {
       name: "Sourav",
-      //   lastname: "Kumar",
-      date: "17/12/2001 | 05:00",
+      lastname: "Kumar",
+      date: "17/12/2001",
+      time: "11 AM",
 
-      specialist: "Neuroscience",
+      disease: "Neuroscience",
+      src: "./Images/YoungMan.png",
+    },
+    {
+      name: "Sourav",
+      lastname: "Kumar",
+      date: "17/12/2001",
+      time: "11 AM",
+
+      disease: "Neuroscience",
+      src: "./Images/YoungMan.png",
+    },
+    {
+      name: "Sourav",
+      lastname: "Kumar",
+      date: "17/12/2001",
+      time: "11 AM",
+
+      disease: "Neuroscience",
+      src: "./Images/YoungMan.png",
+    },
+    {
+      name: "Sourav",
+      lastname: "Kumar",
+      date: "17/12/2001",
+      time: "11 AM",
+
+      disease: "Neuroscience",
       src: "./Images/YoungMan.png",
     },
     {
       name: "Sourav",
       //   lastname: "Kumar",
-      date: "17/12/2001 | 05:00",
+      date: "17/12/2001",
+      time: "11 AM",
 
-      specialist: "Neuroscience",
+      disease: "Neuroscience",
       src: "./Images/YoungMan.png",
     },
     {
       name: "Sourav",
-      //   lastname: "Kumar",
-      date: "17/12/2001 | 05:00",
+      lastname: "Kumar",
+      date: "17/12/2001",
+      time: "11 AM",
 
-      specialist: "Neuroscience",
-      src: "./Images/YoungMan.png",
-    },
-    {
-      name: "Sourav",
-      //   lastname: "Kumar",
-      date: "17/12/2001 | 05:00",
-
-      specialist: "Neuroscience",
-      src: "./Images/YoungMan.png",
-    },
-    {
-      name: "Sourav",
-      //   lastname: "Kumar",
-      date: "17/12/2001 | 05:00",
-
-      specialist: "Neuroscience",
-      src: "./Images/YoungMan.png",
-    },
-    {
-      name: "Sourav",
-      //   lastname: "Kumar",
-      date: "17/12/2001 | 05:00",
-
-      specialist: "Neuroscience",
+      disease: "Neuroscience",
       src: "./Images/YoungMan.png",
     },
     {
       name: "Yashraj",
-      //   lastname: "Kumar",
-      date: "17/12/2001 | 05:00",
+      lastname: "Kumar",
+      date: "17/12/2001",
+      time: "11 AM",
 
-      specialist: "Cardiology",
+      disease: "Cardiology",
       src: "./Images/YoungMan.png",
     },
     {
       name: "Tathagat",
-      //   lastname: "Kumar",
-      date: "17/12/2001 | 05:00",
+      lastname: "Kumar",
+      date: "17/12/2001",
+      time: "11 AM",
 
-      specialist: "Surgeon",
+      disease: "Surgeon",
       src: "./Images/YoungMan.png",
     },
   ];
   return (
     <>
       <DocNav />
-      <div className="flex flex-row justify-around  p-5 bg-pink-400">
-        <div className="w-5/12 bg-cyan-500 h-fit rounded-3xl sm:mt-9 mt-16">
+      <div className="flex flex-row justify-around  p-5 bg-pink-400 ">
+        <div className="w-5/12 bg-cyan-500 h-fit rounded-3xl sm:mt-5 mt-5">
           <div className="bg-white p-3 rounded-3xl">
             <div className="flex justify-center">
               <img
@@ -82,57 +90,69 @@ const Profile = () => {
               ></img>
             </div>
             <div className="flex justify-center p-3 font-bold font-serif">
-                Dr. Yashraj Srivastava
+              Dr. Yashraj Srivastava
             </div>
             <div className="flex justify-center p-3 font-thin font-serif text-gray-500">
-                MBBS, MS, MD, PhD
+              MBBS, MS, MD, PhD
             </div>
             <div className="flex justify-center p-3 font-bold font-serif">
-                <span className="font-thin">ID-</span><b>SNJ24066998G</b>
+              <span className="font-thin">ID-</span>
+              <b>SNJ24066998G</b>
             </div>
             <div className="flex justify-center p-3  font-serif ">
-                Neurosurgeon
+              Neurosurgeon
             </div>
             <div className="flex justify-center">
-            <button className="bg-blue-900 text-2xl text-white p-3 rounded-3xl font-bold ">
+              <button className="bg-blue-900 text-2xl text-white p-3 rounded-3xl font-bold ">
                 Update
               </button>
             </div>
-            
           </div>
         </div>
+        <div className="w-6/12 bg-white rounded-3xl mt-5 overflow-y-auto">
+          <div className="font-bold text-2xl p-3">Appointments</div>
+          <div className="h-[70vh]">
+        {doctor.map(({name,lastname,date,time,disease,src})=>(
+          <div className="flex flex-col mb-3 ">
+           <div className="flex flex-col px-6 ">
+           <div className=" flex flex-row rounded-3xl border-r-8 border-b-8">
+             <div className="flex flex-row justify-between w-full rounded-3xl border-3">
+               <div className="flex flex-row ">
+                 <div className="flex flex-col justify-center size-28 px-3 ">
+                   <img
+                     className="rounded-3xl"
+                     src={src}
+                     alt="No profile"
+                   />
+                 </div>
+                 <div className="flex flex-col justify-center">
+                   <div className="text-xl">
+                     {name} <b className="">{lastname}</b>
+                   </div>
+                   <div className="flex sm:flex-row flex-col">
+                     <div className="mr-1">
+                       <b>{date}</b>
+                     </div>
+                     <div className="text-gray-400">{time}</div>
+                   </div>
+                   <div className="text-gray-700">{disease}</div>
+                 </div>
+               </div>
+               
+               <div className="flex flex-row">
+                 <div className="flex flex-col justify-center mr-8"><img src="./Images/message.png" alt="No message" /></div>
+                 <div className="flex flex-col justify-center"><img className="h-[35%]" src="./Images/arrow.png" alt="No arrow"/></div>
+               </div>
+               
+             </div>
+           </div>
+         </div>
+         </div>
+        ))}
         
-          <div 
-            className="w-6/12  relative h-[80vh] bg-white ml-10 mt-5 mr-10 mb-10 rounded-3xl flex flex-col overflow-visible border-1 border-gray-500"
-            style={{ overflowY: "auto" }}
-          >
-            <div className="text-sm text-black font-inter font-semibold md:text-3xl mt-3 md:ml-8 ml">
-              Appointments
-            </div>
-            <div className="overflow-y-auto h-auto">
-              {doctor.map(({ name, specialist, src, date }) => (
-                <div className="flex flex-col">
-                  <div className=" ml-4 mt-5 w-4/12 rounded:xl h-4/12 flex flex-row lg:bg-slate-300 md:w-11/12 md:h-5/6 md:rounded-3xl md:ml-8 md:mt-8 ">
-                    <img
-                      className="md:rounded-full h-5/6 md:w-24 md:ml-5 mt-4 w-16 ml-1 "
-                      src={src}
-                    />
-                    <div className="font-thin ml-1 text-sm text-black font-inter md:font-medium md:text-3xl md:mt-2 md:ml-6 flex flex-col">
-                      {name}
-                      <div className="md:text-lg md:mt-[-4px] md:font-normal text-sm font-extralight mr-9">
-                        {date}
-                      </div>
-                      <div className="md:text-lg font-serif text-sm font-thin">
-                        {specialist}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
+         </div>
         </div>
-      
+      </div>
     </>
   );
 };
